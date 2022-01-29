d5e3891fcc6bb3201e04bf380da707253cee2d34
Sandeep_jaina <sandeep.jaina@outlook.com>
2022-01-29 07:09:36 -0800

    asfafas

--- Terraform/Testing/EC2.tf
+++ Terraform/Testing/EC2.tf
@@ -7,7 +7,7 @@ resource "aws_spot_instance_request" "cheap_worker" {
   wait_for_fulfillment      = true
   //spot_type                 = "persistent"
   tags                      = {
-    Name                    = "${element(var.COMPONENTS, count.index)}"-Dev
+    Name                    = "element(var.COMPONENTS, count.index)"-Dev
   }
 }
 
@@ -15,7 +15,7 @@ resource "aws_ec2_tag" "name-tag" {
   count                     = local.LENGTH
   resource_id               = element(aws_spot_instance_request.cheap_worker.*.spot_instance_id, count.index)
   key                       = "Name"
-  value                     = "${element(var.COMPONENTS, count.index)}"-Dev
+  value                     = "element(var.COMPONENTS, count.index)"-Dev
 }
 
 
