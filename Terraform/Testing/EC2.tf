d2c334aadf4c8bec2efa647553cd9b62b8e340fe
Sandeep_jaina <sandeep.jaina@outlook.com>
2022-01-29 08:05:57 -0800

    asfafas

--- Terraform/Testing/EC2.tf
+++ Terraform/Testing/EC2.tf
@@ -1,7 +1,7 @@
 resource "aws_spot_instance_request" "cheap_worker" {
   count                     = local.LENGTH
   ami                       = "ami-0fc800a3140d9997d"
-  spot_price                = "0.0031"
+  //spot_price                = "0.0031"
   instance_type             = "t3.micro"
   vpc_security_group_ids    = ["sg-03a75d657159b8990"]
   wait_for_fulfillment      = true
